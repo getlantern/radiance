ff24cf0f5b339edcc8dc3d7a04de738e248c2ec2
garmr <pdixon117@gmail.com>
2024-12-09 12:24:47 -0800

    use local server for dialer test

--- transport/shadowsocks/stream_dialer_test.go
+++ transport/shadowsocks/stream_dialer_test.go
@@ -2,59 +2,87 @@ package shadowsocks
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"net"
-	"net/http"
-	"net/url"
+	"strconv"
 	"testing"
-	"time"
 
 	"github.com/Jigsaw-Code/outline-sdk/transport"
 	"github.com/stretchr/testify/require"
 
 	"github.com/getlantern/radiance/config"
-	"github.com/getlantern/radiance/transport/multiplex"
 )
 
-func TestWrapStreamDialer(t *testing.T) {
-	config, err := config.GetConfig()
-	require.NoError(t, err, "Failed to get config")
+func TestStreamDialer(t *testing.T) {
+	done := make(chan error, 1)
+	listener, err := startServer(done)
+	require.NoError(t, err)
+	defer listener.Close()
 
-	dialer, err := NewStreamDialer(&transport.TCPDialer{}, config)
-	require.NoError(t, err, "Failed to wrap stream dialer")
+	conf, err := newTestConfig(listener.Addr().String())
+	require.NoError(t, err)
 
-	dialer, _ = multiplex.NewStreamDialer(dialer, config)
+	dialer, err := NewStreamDialer(&transport.TCPDialer{}, conf)
+	conn, err := dialer.DialStream(context.Background(), listener.Addr().String())
+	require.NoError(t, err)
 
-	header := http.Header{}
-	header.Add("X-Lantern-Auth-Token", config.AuthToken)
-	addr := fmt.Sprintf("%s:%d", config.Addr, config.Port)
-	client := &http.Client{
-		Transport: &http.Transport{
-			DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
-				return dialer.DialStream(ctx, addr)
-			},
-			Proxy:              http.ProxyURL(&url.URL{Scheme: "http", Host: addr}),
-			ProxyConnectHeader: header,
-		},
-		Timeout: 10 * time.Second,
+	msg := "Why are you copying me?!"
+	n, err := conn.Write([]byte(msg))
+	require.NoError(t, err)
+	require.Equalf(t, len(msg), n, "wrote %d/%d bytes", n, len(msg))
+
+	// make sure the buffer is large enough to also hold the generated upstream address (upto 22 bytes)
+	read := make([]byte, len(msg)*2+22)
+	n, err = conn.Read(read)
+	if !errors.Is(err, io.EOF) {
+		require.NoError(t, err)
+	}
+	require.Greaterf(t, n, len(msg), "read %d <%d bytes", n, len(msg))
+	require.Equal(t, msg, string(read[n-len(msg):n]))
+
+	conn.Close()
+	<-done
+}
+
+// startServer starts a server that echos back any data sent to it.
+func startServer(done chan error) (net.Listener, error) {
+	listener, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		return nil, fmt.Errorf("Could not listen: %w", err)
 	}
 
-	req, err := http.NewRequest("GET", "https://geo.getiantem.org/lookup/185.228.19.20", nil)
-	require.NoError(t, err, "Failed to create request")
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				done <- fmt.Errorf("Error accepting connection: %w", err)
+				return
+			}
 
-	resp, err := client.Do(req)
-	require.NoError(t, err, "Failed to make request")
-
-	for key, values := range resp.Header {
-		for _, value := range values {
-			fmt.Printf("%s: %s\n", key, value)
+			// Echo back
+			io.Copy(conn, conn)
+			conn.Close()
+			done <- nil
 		}
+	}()
+
+	return listener, nil
+}
+
+func newTestConfig(addr string) (config.Config, error) {
+	addr, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		return config.Config{}, err
 	}
-
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	require.NoError(t, err, "Failed to read response body")
-
-	t.Log(string(body))
+	p, _ := strconv.Atoi(port)
+	return config.Config{
+		Addr: addr,
+		Port: p,
+		Shadowsocks: map[string]string{
+			"cipher": "aes-256-gcm",
+			"secret": "test",
+		},
+	}, nil
 }
