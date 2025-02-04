60a5e1e5de8a172b5c25736563ebf37d0037d9bd
Wendel Hime <6754291+WendelHime@users.noreply.github.com>
2025-02-04 20:37:35 -0300

    Implementing minimum API for clients (#7)
    * feat: drafting minimal API manager interface based on specs
    
    * fix: setting type of VPN status constants
    
    * feat: adding Shutdown and function for retrieving current config
    
    * feat: adding proxy server api implementation
    
    * fix: removing temp config and use config handler for retrieving config
    
    * fix: renaming files and structs and also update active location to receive a context for fetching config
    
    * fix: removing added field
    
    * fix: updating active location tests
    
    * feat: adding consumption tracker transport
    
    * feat: retrieving data usage
    
    * feat: adding data cap var with comment explaining why is set to 0
    
    * feat: generating stream dialer mocks and testing consumption transport
    
    * feat: removing bandwidth status for now since we don't have a radiance device ID, without this we can't retrieve the bandwidth from the back-end
    
    * feat: removing some bandwidth forgotten parameters and add ProxyStatus func
    
    * feat: moving functions to Radiance struct
    
    * feat: verify if http server is nil before shutting down
    
    * feat: using ticker instead of time.After
    
    * feat: add TUNStatus and connected flag to radiance
    
    * fix: moving ProxyStatus and TUNStatus to radiance.go
    
    * fix: making ActiveProxyLocation return a string, logging errors and also returning empty string when location unavailable
    
    * fix: removing api.go since consumers can create the interface
    
    * fix: updating proxy status listeners based on set status calls
    
    * fix: updating ProxyStatus documentation to reflect that it returns a channel that receives a message whenever a status update happens

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/getlantern/kindling v0.0.0-20250131224031-b92afbdcc7e0
 	github.com/stretchr/testify v1.10.0
 	github.com/xtaci/smux v1.5.31
+	go.uber.org/mock v0.5.0
 	google.golang.org/protobuf v1.35.1
 )
 
--- radiance.go
+++ radiance.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/getlantern/golog"
@@ -24,15 +25,64 @@ var (
 	configPollInterval = 10 * time.Minute
 )
 
+//go:generate mockgen -destination=radiance_mock_test.go -package=radiance github.com/getlantern/radiance httpServer,configHandler
+
+// httpServer is an interface that abstracts the http.Server struct for easier testing.
+type httpServer interface {
+	Serve(listener net.Listener) error
+	Shutdown(ctx context.Context) error
+}
+
+// configHandler is an interface that abstracts the config.ConfigHandler struct for easier testing.
+type configHandler interface {
+	// GetConfig returns the current proxy configuration.
+	GetConfig(ctx context.Context) (*config.Config, error)
+	// Stop stops the config handler from fetching new configurations.
+	Stop()
+}
+
+// TUNStatus is a type used for representing the state of the TUN device and routing configuration.
+type TUNStatus string
+
+const (
+	ConnectedTUNStatus    TUNStatus = "connected"
+	DisconnectedTUNStatus TUNStatus = "disconnected"
+	ConnectingTUNStatus   TUNStatus = "connecting"
+)
+
+// ProxyStatus provide
+type ProxyStatus struct {
+	Connected bool
+	// Location provides the proxy's geographical location. If connected is false,
+	// the value will be a empty string.
+	Location string
+}
+
 // Radiance is a local server that proxies all requests to a remote proxy server over a transport.StreamDialer.
+// TODO: tunStatus need to be updated when TUN is active
 type Radiance struct {
-	srv         *http.Server
-	confHandler *config.ConfigHandler
+	srv         httpServer
+	confHandler configHandler
+
+	connected              bool
+	tunStatus              TUNStatus
+	statusMutex            sync.Locker
+	stopChan               chan struct{}
+	proxyStatusListenersMu sync.Locker
+	proxyStatusListeners   []chan ProxyStatus
 }
 
 // NewRadiance creates a new Radiance server using an existing config.
 func NewRadiance() *Radiance {
-	return &Radiance{confHandler: config.NewConfigHandler(configPollInterval)}
+	return &Radiance{
+		confHandler:            config.NewConfigHandler(configPollInterval),
+		connected:              false,
+		tunStatus:              DisconnectedTUNStatus,
+		statusMutex:            new(sync.Mutex),
+		proxyStatusListenersMu: new(sync.Mutex),
+		stopChan:               make(chan struct{}),
+		proxyStatusListeners:   make([]chan ProxyStatus, 0),
+	}
 }
 
 // Run starts the Radiance proxy server on the specified address.
@@ -41,11 +91,13 @@ func (r *Radiance) Run(addr string) error {
 	conf, err := r.confHandler.GetConfig(ctx)
 	cancel()
 	if err != nil {
+		r.setStatus(false, r.TUNStatus())
 		return err
 	}
 
 	dialer, err := transport.DialerFrom(conf)
 	if err != nil {
+		r.setStatus(false, r.TUNStatus())
 		return fmt.Errorf("Could not create dialer: %w", err)
 	}
 	log.Debugf("Creating dialer with config: %+v", conf)
@@ -63,6 +115,8 @@ func (r *Radiance) Run(addr string) error {
 		},
 	}
 	r.srv = &http.Server{Handler: &handler}
+
+	r.setStatus(true, r.TUNStatus())
 	return r.listenAndServe(addr)
 }
 
@@ -74,4 +128,96 @@ func (r *Radiance) listenAndServe(addr string) error {
 
 	log.Debugf("Listening on %v", addr)
 	return r.srv.Serve(listener)
+}
+
+// Shutdown stops the Radiance server.
+func (r *Radiance) Shutdown(ctx context.Context) error {
+	if !r.connectionStatus() {
+		return nil
+	}
+	if r.srv == nil {
+		return fmt.Errorf("server is nil")
+	}
+	if err := r.srv.Shutdown(ctx); err != nil {
+		return fmt.Errorf("failed to shutdown server: %w", err)
+	}
+	r.confHandler.Stop()
+	r.setStatus(false, r.TUNStatus())
+	close(r.stopChan)
+	return nil
+}
+
+func (r *Radiance) connectionStatus() bool {
+	r.statusMutex.Lock()
+	defer r.statusMutex.Unlock()
+	return r.connected
+}
+
+func (r *Radiance) setStatus(connected bool, status TUNStatus) {
+	r.statusMutex.Lock()
+	r.connected = connected
+	r.tunStatus = status
+	r.statusMutex.Unlock()
+
+	// send notifications in a separate goroutine to avoid blocking the Radiance main loop
+	go r.notifyListeners(connected)
+}
+
+func (r *Radiance) notifyListeners(connected bool) {
+	r.proxyStatusListenersMu.Lock()
+	status := ProxyStatus{
+		Connected: connected,
+		Location:  r.ActiveProxyLocation(context.Background()),
+	}
+	r.proxyStatusListenersMu.Unlock()
+	for _, listener := range r.proxyStatusListeners {
+		select {
+		case listener <- status:
+		default:
+		}
+	}
+}
+
+// TUNStatus checks the current TUN status
+func (r *Radiance) TUNStatus() TUNStatus {
+	r.statusMutex.Lock()
+	defer r.statusMutex.Unlock()
+	return r.tunStatus
+}
+
+// ActiveProxyLocation returns the proxy server's location if the VPN is connected.
+// If the VPN is disconnected, it returns nil.
+func (r *Radiance) ActiveProxyLocation(ctx context.Context) string {
+	if !r.connectionStatus() {
+		log.Debug("VPN is not connected")
+		return ""
+	}
+
+	config, err := r.confHandler.GetConfig(ctx)
+	if err != nil {
+		log.Errorf("could not retrieve config: %w", err)
+		return ""
+	}
+
+	if config == nil {
+		log.Errorf("config is nil")
+		return ""
+	}
+
+	if location := config.GetLocation(); location != nil {
+		return location.City
+	}
+	log.Errorf("could not retrieve location")
+	return ""
+}
+
+// ProxyStatus returns a channel that is populated whenever the proxy status changes.
+// It provides information about the current proxy status like the proxy's
+// location or whether the proxy is connected or not.
+func (r *Radiance) ProxyStatus() <-chan ProxyStatus {
+	proxyStatus := make(chan ProxyStatus)
+	r.proxyStatusListenersMu.Lock()
+	r.proxyStatusListeners = append(r.proxyStatusListeners, proxyStatus)
+	r.proxyStatusListenersMu.Unlock()
+	return proxyStatus
 }
