3efaed756001d8ae6a7b79cde1f897d36cb5b40f
garmr <pdixon117@gmail.com>
2025-02-16 16:49:01 -0800

    use custom dialer with sing-box

---
+++ client/rules.go
@@ -0,0 +1,175 @@
+package client
+
+import (
+	"net/netip"
+	"os"
+
+	"github.com/sagernet/sing-box/option"
+	"github.com/sagernet/sing/common/json"
+	"github.com/sagernet/sing/common/json/badoption"
+)
+
+func getopts() option.Options {
+	var options = option.Options{
+		Log: &option.LogOptions{
+			Disabled: true,
+		},
+		// DNS: &option.DNSOptions{
+		// 	Servers: []option.DNSServerOptions{
+		// 		{
+		// 			Tag:      "proxy",
+		// 			Address:  "192.1.2.11",
+		// 			Detour:   "proxy",
+		// 			Strategy: option.DomainStrategy(dns.DomainStrategyUseIPv4),
+		// 		},
+		// 		{
+		// 			Tag:      "local",
+		// 			Address:  "192.1.2.11",
+		// 			Detour:   "direct",
+		// 			Strategy: option.DomainStrategy(dns.DomainStrategyUseIPv4),
+		// 		},
+		// 	},
+		// 	Rules: rules,
+		// 	DNSClientOptions: option.DNSClientOptions{
+		// 		DisableCache: true,
+		// 	},
+		// },
+		Inbounds: []option.Inbound{
+			{
+				Type: "tun",
+				Tag:  "tun-in",
+				Options: &option.TunInboundOptions{
+					InterfaceName: "utun225",
+					Address: badoption.Listable[netip.Prefix]{
+						netip.MustParsePrefix("10.10.1.1/30"),
+					},
+					AutoRoute:   true,
+					StrictRoute: false,
+					// EndpointIndependentNat: true,
+					Stack: "system",
+				},
+			},
+		},
+		Outbounds: []option.Outbound{
+			{
+				Type: "direct",
+				Tag:  "direct",
+			},
+			{
+				Type: "algeneva",
+				Tag:  "algeneva-out",
+			},
+		},
+		Route: &option.RouteOptions{
+			AutoDetectInterface: true,
+			Rules: []option.Rule{
+				{
+					Type: "default",
+					DefaultOptions: option.DefaultRule{
+						RawDefaultRule: option.RawDefaultRule{
+							Inbound: badoption.Listable[string]{"tun-in"},
+						},
+						RuleAction: option.RuleAction{
+							Action:       "sniff",
+							RouteOptions: option.RouteActionOptions{},
+						},
+					},
+				},
+				{
+					Type: "default",
+					DefaultOptions: option.DefaultRule{
+						RawDefaultRule: option.RawDefaultRule{
+							Inbound: badoption.Listable[string]{"tun-in"},
+							Domain:  badoption.Listable[string]{"ipconfig.io"},
+						},
+						RuleAction: option.RuleAction{
+							Action: "route",
+							RouteOptions: option.RouteActionOptions{
+								Outbound: "algeneva-out",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	options.Log = &option.LogOptions{
+		Disabled:     false,
+		Level:        "trace",
+		Output:       "debug.log",
+		Timestamp:    true,
+		DisableColor: true,
+	}
+	return options
+}
+
+var rules = []option.DNSRule{
+	// not used currently
+	{
+		Type: "default",
+		DefaultOptions: option.DefaultDNSRule{
+			RawDefaultDNSRule: option.RawDefaultDNSRule{
+				Inbound:                             []string{"tun-in"},
+				IPVersion:                           4,
+				QueryType:                           []option.DNSQueryType{},
+				Network:                             []string{},
+				AuthUser:                            []string{},
+				Protocol:                            []string{},
+				Domain:                              []string{"ipconfig.io"},
+				DomainSuffix:                        []string{},
+				DomainKeyword:                       []string{},
+				DomainRegex:                         []string{},
+				Geosite:                             []string{"us"},
+				SourceGeoIP:                         []string{},
+				GeoIP:                               []string{},
+				IPCIDR:                              []string{},
+				IPIsPrivate:                         false,
+				SourceIPCIDR:                        []string{},
+				SourceIPIsPrivate:                   false,
+				SourcePort:                          []uint16{},
+				SourcePortRange:                     []string{},
+				Port:                                []uint16{},
+				PortRange:                           []string{},
+				ProcessName:                         []string{},
+				ProcessPath:                         []string{},
+				ProcessPathRegex:                    []string{},
+				PackageName:                         []string{},
+				User:                                []string{},
+				UserID:                              []int32{},
+				Outbound:                            []string{},
+				ClashMode:                           "",
+				NetworkType:                         []option.InterfaceType{},
+				NetworkIsExpensive:                  false,
+				NetworkIsConstrained:                false,
+				WIFISSID:                            []string{},
+				WIFIBSSID:                           []string{},
+				RuleSet:                             []string{},
+				RuleSetIPCIDRMatchSource:            false,
+				RuleSetIPCIDRAcceptEmpty:            false,
+				Invert:                              false,
+				Deprecated_RulesetIPCIDRMatchSource: false,
+			},
+			DNSRuleAction: option.DNSRuleAction{
+				Action: "route",
+				RouteOptions: option.DNSRouteActionOptions{
+					Server:       "local",
+					DisableCache: false,
+					RewriteTTL:   new(uint32),
+					ClientSubnet: nil,
+				},
+				RouteOptionsOptions: option.DNSRouteOptionsActionOptions{},
+				RejectOptions:       option.RejectActionOptions{},
+			},
+		},
+		LogicalOptions: option.LogicalDNSRule{},
+	},
+}
+
+func save(opts option.Options) {
+	b, err := json.Marshal(opts)
+	if err != nil {
+		glog.Fatal(err)
+	}
+	os.WriteFile("client/opts.json", b, 0644)
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
-	"flag"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/getlantern/golog"
 
-	"github.com/getlantern/radiance"
+	"github.com/getlantern/radiance/client"
 )
 
 var log = golog.LoggerFor("main")
 
 func main() {
-	addrFlag := flag.String("addr", "localhost:8080", "Address to listen on")
-	flag.Parse()
+	// addrFlag := flag.String("addr", "localhost:8080", "Address to listen on")
+	// flag.Parse()
+	//
+	// rad := radiance.NewRadiance()
+	// if err := rad.Run(*addrFlag); err != nil {
+	// 	log.Fatalf("Failed to run radiance: %v", err)
+	// }
 
-	rad := radiance.NewRadiance()
-	if err := rad.Run(*addrFlag); err != nil {
-		log.Fatalf("Failed to run radiance: %v", err)
+	b, err := client.NewBox()
+	if err != nil {
+		log.Fatalf("Failed to create box: %v", err)
 	}
+	b.Close()
+	err = b.Start()
+	if err != nil {
+		log.Fatalf("Failed to start box: %v", err)
+	}
+	log.Debug("Box started")
+	defer func() {
+		log.Debug("Box stopping")
+		err := b.Close()
+		if err != nil {
+			log.Errorf("Failed to close box: %v", err)
+		}
+		log.Debug("Box stopped")
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
+	<-sig
 }
