c97055ea4009dce119e6e9bfece6fa45806b6b92
WendelHime <6754291+WendelHime@users.noreply.github.com>
2025-02-24 11:46:21 -0300

    feat: moving function for registering outbounds into registry.go

---
+++ client/registry.go
@@ -0,0 +1,11 @@
+package client
+
+import (
+	"github.com/getlantern/radiance/outbounds/proxyless"
+	"github.com/sagernet/sing-box/adapter/outbound"
+)
+
+func RegisterOutbound(registry *outbound.Registry) {
+	outbound.Register[AlgenevaOutboundOptions](registry, "algeneva", NewOutbound)
+	outbound.Register[proxyless.ProxylessOutboundOptions](registry, "proxyless", proxyless.NewOutbound)
+}
