7bd512d02e8c682fe274c70cca661dcb7e52743c
WendelHime <6754291+WendelHime@users.noreply.github.com>
2025-03-06 17:11:20 -0300

    docs: explaining why the assertion was commented from tests so we can enable it again in the future

--- radiance_test.go
+++ radiance_test.go
@@ -132,7 +132,11 @@ func TestNewRadiance(t *testing.T) {
 		r, err := NewRadiance()
 		assert.NoError(t, err)
 		assert.NotNil(t, r)
-		//assert.NotNil(t, r.confHandler)
+		// Currently we're not creating a config handler when starting radiance,
+		// due to recent changes for using sing-box. We will enable it again
+		// in the future for retrieving the sing-box configuration options
+		// and this should be re-enabled.
+		// assert.NotNil(t, r.confHandler)
 		assert.Nil(t, r.srv)
 		assert.False(t, r.connected)
 		assert.NotNil(t, r.statusMutex)
