858d882e5a28bbd264bab0ec056a77c7fd33c600
atavism <atavism@users.noreply.github.com>
2025-03-25 14:20:38 -0700

    Fix app crash on macOS when starting/stopping VPN multiple times (#51)
    * updates to BoxService
    
    * clean-ups
    
    * clear libbox reference
    
    * Update client/service/service.go
    
    * Replace atomic.Bool with bool for isRunning
    
    * Remove unused sync/atomic import
    
    ---------
    
    Co-authored-by: garmr-ulfr <104022054+garmr-ulfr@users.noreply.github.com>

--- client/service/service.go
+++ client/service/service.go
@@ -27,24 +27,24 @@ import (
 
 // BoxService is a wrapper around libbox.BoxService
 type BoxService struct {
-	libbox *libbox.BoxService
-	ctx    context.Context
-
+	libbox       *libbox.BoxService
+	ctx          context.Context
+	config       string
+	platIfce     libbox.PlatformInterface
 	pauseManager pause.Manager
 	pauseAccess  sync.Mutex
 	pauseTimer   *time.Timer
+	mu           sync.Mutex
+	isRunning    bool
 }
 
 // New creates a new BoxService that wraps a [libbox.BoxService]. platformInterface is used
 // to interact with the underlying platform
 func New(config, dataDir string, platIfce libbox.PlatformInterface) (*BoxService, error) {
-	inboundRegistry, outboundRegistry, endpointRegistry := protocol.GetRegistries()
-	ctx := box.Context(
-		context.Background(),
-		inboundRegistry,
-		outboundRegistry,
-		endpointRegistry,
-	)
+	bs := &BoxService{
+		config:   config,
+		platIfce: platIfce,
+	}
 	setupOpts := &libbox.SetupOptions{
 		BasePath:    dataDir,
 		WorkingPath: filepath.Join(dataDir, "data"),
@@ -56,27 +56,78 @@ func New(config, dataDir string, platIfce libbox.PlatformInterface) (*BoxService
 	if err := libbox.Setup(setupOpts); err != nil {
 		return nil, fmt.Errorf("setup libbox: %w", err)
 	}
-	lb, err := libbox.NewServiceWithContext(ctx, config, platIfce)
-	if err != nil {
-		return nil, fmt.Errorf("create libbox service: %w", err)
-	}
-
-	bs := &BoxService{
-		libbox:       lb,
-		ctx:          ctx,
-		pauseManager: service.FromContext[pause.Manager](ctx),
-		pauseAccess:  sync.Mutex{},
-	}
 
 	return bs, nil
 }
 
 func (bs *BoxService) Start() error {
-	return bs.libbox.Start()
+	bs.mu.Lock()
+	defer bs.mu.Unlock()
+
+	if bs.isRunning {
+		return errors.New("service is already running")
+	}
+
+	// (re)-initialize the libbox service
+	lb, ctx, err := newLibboxService(bs.config, bs.platIfce)
+	if err != nil {
+		return err
+	}
+
+	bs.libbox = lb
+	bs.ctx = ctx
+	bs.pauseManager = service.FromContext[pause.Manager](ctx)
+
+	if err = lb.Start(); err == nil {
+		bs.isRunning = true
+	}
+	return err
+}
+
+// newLibboxService creates a new libbox service with the given config and platform interface
+func newLibboxService(config string, platIfce libbox.PlatformInterface) (*libbox.BoxService, context.Context, error) {
+	// Retrieve protocol registries
+	inboundRegistry, outboundRegistry, endpointRegistry := protocol.GetRegistries()
+	ctx := box.Context(
+		context.Background(),
+		inboundRegistry,
+		outboundRegistry,
+		endpointRegistry,
+	)
+
+	// initialize the libbox service
+	lb, err := libbox.NewServiceWithContext(ctx, config, platIfce)
+	if err != nil {
+		return nil, nil, fmt.Errorf("create libbox service: %w", err)
+	}
+
+	return lb, ctx, nil
 }
 
 func (bs *BoxService) Close() error {
-	return bs.libbox.Close()
+	bs.mu.Lock()
+	defer bs.mu.Unlock()
+
+	if !bs.isRunning {
+		return errors.New("service already stopped")
+	}
+
+	// Clear pause timer
+	if bs.pauseTimer != nil {
+		bs.pauseTimer.Stop()
+		bs.pauseTimer = nil
+	}
+
+	if bs.libbox != nil {
+		err := bs.libbox.Close()
+		if err != nil {
+			return fmt.Errorf("failed to close libbox: %v", err)
+		}
+		bs.libbox = nil
+	}
+
+	bs.isRunning = false
+	return nil
 }
 
 // Pause pauses the network for the specified duration. An error is returned if the network is
@@ -90,7 +141,7 @@ func (bs *BoxService) Pause(dur time.Duration) error {
 	}
 
 	bs.pauseManager.NetworkPause()
-	bs.pauseTimer = time.AfterFunc(dur, bs.pauseManager.NetworkWake)
+	bs.pauseTimer = time.AfterFunc(dur, bs.Wake)
 	return nil
 }
 
@@ -103,6 +154,8 @@ func (bs *BoxService) Wake() {
 		return
 	}
 	bs.pauseManager.NetworkWake()
-	bs.pauseTimer.Stop()
-	bs.pauseTimer = nil
+	if bs.pauseTimer != nil {
+		bs.pauseTimer.Stop()
+		bs.pauseTimer = nil
+	}
 }
