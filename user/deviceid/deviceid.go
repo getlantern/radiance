6a1b70a7bb6c99255eb647aa6e7840a77ffea09f
atavism <paul@getlantern.org>
2025-04-10 09:04:48 -0700

    Fix log issue on windows

--- user/deviceid/deviceid.go
+++ user/deviceid/deviceid.go
@@ -3,7 +3,12 @@ package deviceid
 import (
 	"encoding/base64"
 
+	"github.com/getlantern/golog"
 	"github.com/google/uuid"
+)
+
+var (
+	log = golog.LoggerFor("deviceid")
 )
 
 // OldStyleDeviceID returns the old style of device ID, which is derived from the MAC address.
