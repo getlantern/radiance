470094a35a2282ba56183d9fe323976b7e15eb49
garmr-ulfr <104022054+garmr-ulfr@users.noreply.github.com>
2025-04-10 15:03:32 -0700

    add ability to update outbounds and endpoints during sing-box runtime (#57)
    * add ability to update outbounds and endpoints during sing-box runtime
    
    * collect tags for items to be removed before removing
    
    * clean up removeItems and fix bug

--- client/service/service.go
+++ client/service/service.go
@@ -15,11 +15,15 @@ import (
 	"log/slog"
 	"path/filepath"
 	"runtime"
+	"slices"
 	"sync"
 	"time"
 
 	box "github.com/sagernet/sing-box"
+	"github.com/sagernet/sing-box/adapter"
 	"github.com/sagernet/sing-box/experimental/libbox"
+	"github.com/sagernet/sing-box/log"
+	"github.com/sagernet/sing-box/option"
 	"github.com/sagernet/sing/common/json"
 	"github.com/sagernet/sing/service"
 	"github.com/sagernet/sing/service/pause"
@@ -192,4 +196,173 @@ func (bs *BoxService) ParseConfig(configRaw []byte) (*config.Config, error) {
 		return nil, fmt.Errorf("parse config: %w", err)
 	}
 	return config, nil
+}
+
+var (
+	permanentOutbounds = []string{
+		"direct",
+		"dns",
+		"block",
+	}
+	permanentEndpoints = []string{}
+)
+
+// updateOutboundsEndpoints updates the outbounds and endpoints in the router, skipping any present in
+// [permanentOutbounds] and [permanentEndpoints]. updateOutboundsEndpoints will continue processing
+// the remaining outbounds and endpoints even if an error occurs, returning a single error of all
+// errors encountered.
+func updateOutboundsEndpoints(ctx context.Context, outbounds []option.Outbound, endpoints []option.Endpoint) error {
+	router := service.FromContext[adapter.Router](ctx)
+	if router == nil {
+		return errors.New("router missing from context")
+	}
+	// TODO: reaallyy need to be able to get that logFactory.. -_-
+	//			we need to create our own and add it to the context
+	logFactory := service.FromContext[log.Factory](ctx)
+	var errs error
+	if len(outbounds) > 0 {
+		outboundMgr := service.FromContext[adapter.OutboundManager](ctx)
+		if outboundMgr == nil {
+			errs = errors.Join(errs, errors.New("outbound manager missing from context"))
+		} else {
+			err := updateOutbounds(ctx, outboundMgr, router, logFactory, outbounds, permanentOutbounds)
+			if err != nil {
+				errs = fmt.Errorf("update outbounds: %w", err)
+			}
+		}
+	}
+	if len(endpoints) > 0 {
+		endpointMgr := service.FromContext[adapter.EndpointManager](ctx)
+		if endpointMgr == nil {
+			errs = errors.Join(errs, errors.New("endpoint manager missing from context"))
+		} else {
+			err := updateEndpoints(ctx, endpointMgr, router, logFactory, endpoints, permanentEndpoints)
+			if err != nil {
+				errs = errors.Join(errs, fmt.Errorf("update endpoints: %w", err))
+			}
+		}
+	}
+	return errs
+}
+
+// updateOutbounds syncs the [adapter.OutboundManager] with the provided outbounds. It skips excluded
+// or untagged entries, removes outdated ones, and creates or updates the rest. If any error occurs,
+// updateOutbounds will continue processing the remaining outbounds and return a single error of
+// all errors encountered.
+func updateOutbounds(
+	ctx context.Context,
+	outboundMgr adapter.OutboundManager,
+	router adapter.Router,
+	logFactory log.Factory,
+	outbounds []option.Outbound,
+	excludeTags []string,
+) error {
+	newItems, errs := filterItems(outbounds, excludeTags, func(it option.Outbound) string {
+		return it.Tag
+	})
+
+	errs = errors.Join(errs, removeItems(
+		outboundMgr.Outbounds(),
+		newItems,
+		excludeTags,
+		outboundMgr.Remove,
+	))
+
+	for tag, outbound := range newItems {
+		logger := logFactory.NewLogger(fmt.Sprintf("outbound/%s[%s]", outbound.Type, tag))
+		err := outboundMgr.Create(ctx, router, logger, tag, outbound.Type, outbound.Options)
+		if err != nil {
+			errs = errors.Join(errs, fmt.Errorf("initialize [%v]: %w", tag, err))
+		}
+	}
+
+	return errs
+}
+
+// updateEndpoints syncs the [adapter.EndpointManager] with the provided [option.Endpoint]s. It skips
+// excluded or untagged entries, removes outdated ones, and creates or updates the rest. If any error
+// occurs, updateEndpoints will continue processing the remaining endpoints and return a single error
+// of all errors encountered.
+func updateEndpoints(
+	ctx context.Context,
+	endpointMgr adapter.EndpointManager,
+	router adapter.Router,
+	logFactory log.Factory,
+	endpoints []option.Endpoint,
+	excludeTags []string,
+) error {
+	// filter endpoints that are missing a tag or are excluded
+	newItems, errs := filterItems(endpoints, excludeTags, func(it option.Endpoint) string {
+		return it.Tag
+	})
+
+	if nToRemove := len(endpointMgr.Endpoints()) - len(newItems); nToRemove > 0 {
+		errs = errors.Join(errs, removeItems(
+			endpointMgr.Endpoints(),
+			newItems,
+			excludeTags,
+			endpointMgr.Remove,
+		))
+	}
+
+	for tag, endpoint := range newItems {
+		logger := logFactory.NewLogger(fmt.Sprintf("endpoint/%s[%s]", endpoint.Type, tag))
+		err := endpointMgr.Create(ctx, router, logger, tag, endpoint.Type, endpoint.Options)
+		if err != nil {
+			errs = errors.Join(errs, fmt.Errorf("initialize [%v]: %w", tag, err))
+		}
+	}
+
+	return errs
+}
+
+// filterItems returns a map of items with tags as keys. It filters out items that are missing a tag
+// or are present in excludeTags. An error is returned listing all items that are missing a tag.
+func filterItems[T any](items []T, excludeTags []string, getTag func(T) string) (map[string]T, error) {
+	var errs error
+	filtered := make(map[string]T)
+	for idx, it := range items {
+		switch tag := getTag(it); {
+		case tag == "":
+			errs = errors.Join(errs, fmt.Errorf("missing tag for %T[%d]", it, idx))
+		case slices.Contains(excludeTags, tag):
+		default:
+			filtered[tag] = it
+		}
+	}
+	return filtered, errs
+}
+
+// bA is a one-off interface to allow for generic handling of both [adapter.Outbound] and
+// [adapter.Endpoint] types.
+type bA interface {
+	Tag() string
+	Type() string
+}
+
+// removeItems removes items not present in newItems or excludeTags using the provided remove function.
+// If an error occurs, it continues processing the remaining items and returns a single error of all
+// errors encountered.
+func removeItems[I ~[]T, T bA, O any](
+	items I,
+	newItems map[string]O,
+	excludeTags []string,
+	remove func(string) error,
+) error {
+	var errs error
+	for i := 0; i < len(items); {
+		it := items[i]
+		if bA(it) == bA(nil) {
+			break
+		}
+		tag := it.Tag()
+		if _, ok := newItems[tag]; !ok && !slices.Contains(excludeTags, tag) {
+			if err := remove(tag); err != nil {
+				errs = errors.Join(errs, fmt.Errorf("remove [%v]: %w", tag, err))
+			}
+		} else {
+			i++
+		}
+	}
+  return errs
 }
