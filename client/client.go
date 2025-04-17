a250357123c112679d78374bf74a227ed070f02d
jigar-f <132374182+jigar-f@users.noreply.github.com>
2025-04-17 21:18:24 +0530

    Fix connection status state (#66)
    * Fix connection status state.
    
    * added sync.Mutex
    
    * update naming.

--- client/client.go
+++ client/client.go
@@ -25,6 +25,7 @@ import (
 var (
 	client   *vpnClient
 	clientMu sync.Mutex
+	statusMu sync.Mutex
 )
 
 type Options struct {
@@ -115,6 +116,7 @@ func (c *vpnClient) StartVPN() error {
 	}
 
 	c.started = true
+	c.setConnectionStatus(true)
 	return nil
 }
 
@@ -137,6 +139,7 @@ func (c *vpnClient) StopVPN() error {
 	if errors.Is(ctx.Err(), context.DeadlineExceeded) {
 		return errors.New("box did not stop in time")
 	}
+	c.setConnectionStatus(false)
 	return err
 }
 
@@ -148,8 +151,8 @@ func (c *vpnClient) ConnectionStatus() bool {
 }
 
 func (c *vpnClient) setConnectionStatus(connected bool) {
-	clientMu.Lock()
-	defer clientMu.Unlock()
+	statusMu.Lock()
+	defer statusMu.Unlock()
 	c.connected = connected
 }
 
