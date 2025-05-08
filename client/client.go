3abee6b5db19110347d21ed674f7b0459f1b4125
garmr <pdixon117@gmail.com>
2025-05-08 08:35:32 -0700

    change vpnclient.started to atomic.bool, set to false on stop

--- client/client.go
+++ client/client.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"slices"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/sagernet/sing-box/constant"
@@ -55,7 +56,7 @@ type vpnClient struct {
 	boxService          *boxservice.BoxService
 	splitTunnelHandler  *SplitTunnel
 	customServerManager *boxservice.CustomServerManager
-	started             bool
+	running             atomic.Bool
 	connected           bool
 }
 
@@ -124,7 +125,7 @@ func NewVPNClient(opts Options) (VPNClient, error) {
 func (c *vpnClient) StartVPN() error {
 	clientMu.Lock()
 	defer clientMu.Unlock()
-	if c.started {
+	if c.running.Load() {
 		return errors.New("VPN client is already running")
 	}
 
@@ -140,7 +141,7 @@ func (c *vpnClient) StartVPN() error {
 
 	c.customServerManager.SetContext(c.boxService.Ctx())
 
-	c.started = true
+	c.running.Store(true)
 	c.setConnectionStatus(true)
 	return nil
 }
@@ -149,7 +150,7 @@ func (c *vpnClient) StartVPN() error {
 func (c *vpnClient) StopVPN() error {
 	clientMu.Lock()
 	defer clientMu.Unlock()
-	if !c.started {
+	if !c.running.Load() {
 		return errors.New("VPN client is not running")
 	}
 
@@ -159,6 +160,7 @@ func (c *vpnClient) StopVPN() error {
 	go func() {
 		err = c.boxService.Close()
 		cancel()
+		c.running.Store(false)
 	}()
 	<-ctx.Done()
 	if errors.Is(ctx.Err(), context.DeadlineExceeded) {
@@ -172,7 +174,7 @@ func (c *vpnClient) StopVPN() error {
 func (c *vpnClient) ConnectionStatus() bool {
 	clientMu.Lock()
 	defer clientMu.Unlock()
-	return c.started && c.connected
+	return c.running.Load() && c.connected
 }
 
 func (c *vpnClient) setConnectionStatus(connected bool) {
