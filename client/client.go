73f269887e1dd591e3c4e72631e77ad31c3d0115
garmr-ulfr <104022054+garmr-ulfr@users.noreply.github.com>
2025-05-08 08:43:59 -0700

    Set started false (#91)
    * change vpnclient.started to atomic.bool and set to false on stop

--- client/client.go
+++ client/client.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"slices"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/sagernet/sing-box/constant"
@@ -55,7 +56,7 @@ type vpnClient struct {
 	boxService          *boxservice.BoxService
 	splitTunnelHandler  *SplitTunnel
 	customServerManager *boxservice.CustomServerManager
-	started             bool
+	running             atomic.Bool
 	connected           bool
 }
 
@@ -122,11 +123,12 @@ func NewVPNClient(opts Options) (VPNClient, error) {
 
 // Start starts the VPN client
 func (c *vpnClient) StartVPN() error {
-	clientMu.Lock()
-	defer clientMu.Unlock()
-	if c.started {
+	if c.running.Load() {
 		return errors.New("VPN client is already running")
 	}
+
+	clientMu.Lock()
+	defer clientMu.Unlock()
 
 	slog.Debug("Starting VPN client")
 	if c.boxService == nil {
@@ -140,18 +142,19 @@ func (c *vpnClient) StartVPN() error {
 
 	c.customServerManager.SetContext(c.boxService.Ctx())
 
-	c.started = true
+	c.running.Store(true)
 	c.setConnectionStatus(true)
 	return nil
 }
 
 // Stop stops the VPN client and closes the TUN device
 func (c *vpnClient) StopVPN() error {
-	clientMu.Lock()
-	defer clientMu.Unlock()
-	if !c.started {
+	if !c.running.Load() {
 		return errors.New("VPN client is not running")
 	}
+
+	clientMu.Lock()
+	defer clientMu.Unlock()
 
 	slog.Debug("Stopping VPN client")
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
@@ -159,6 +162,7 @@ func (c *vpnClient) StopVPN() error {
 	go func() {
 		err = c.boxService.Close()
 		cancel()
+		c.running.Store(false)
 	}()
 	<-ctx.Done()
 	if errors.Is(ctx.Err(), context.DeadlineExceeded) {
@@ -172,7 +176,7 @@ func (c *vpnClient) StopVPN() error {
 func (c *vpnClient) ConnectionStatus() bool {
 	clientMu.Lock()
 	defer clientMu.Unlock()
-	return c.started && c.connected
+	return c.running.Load() && c.connected
 }
 
 func (c *vpnClient) setConnectionStatus(connected bool) {
