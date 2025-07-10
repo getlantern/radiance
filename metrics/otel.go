e24250376c09e4059c5f97a1e9b327ea0d4fed95
Adam Fisk <afisk@getlantern.org>
2025-07-09 21:56:59 -0600

    More cleanups

--- metrics/otel.go
+++ metrics/otel.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net"
+	"log"
+	"log/slog"
 	"time"
 
 	"github.com/getlantern/common"
@@ -12,17 +13,17 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-	"go.opentelemetry.io/otel/propagation"
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 // SetupOTelSDK bootstraps the OpenTelemetry pipeline.
 // If it does not return an error, make sure to call shutdown for proper cleanup.
-func SetupOTelSDK(ctx context.Context, cfg common.OTEL, contextDialer func(ctx context.Context, addr string) (net.Conn, error)) (func(context.Context) error, error) {
+func SetupOTelSDK(ctx context.Context, cfg common.OTEL) (func(context.Context) error, error) {
 	var shutdownFuncs []func(context.Context) error
 	if !cfg.TracesEnabled && !cfg.MetricsEnabled {
 		// No need to initialize anything if all are disabled.
@@ -36,11 +37,6 @@ func SetupOTelSDK(ctx context.Context, cfg common.OTEL, contextDialer func(ctx c
 		shutdownFuncs = nil
 		return err
 	}
-	conn, err := initConn(cfg.Endpoint, contextDialer)
-	if err != nil {
-		return shutdown, fmt.Errorf("failed to initialize gRPC connection: %w", err)
-	}
-	shutdownFuncs = append(shutdownFuncs, func(_ context.Context) error { return conn.Close() })
 
 	res, err := resource.New(ctx,
 		resource.WithAttributes(
@@ -52,19 +48,21 @@ func SetupOTelSDK(ctx context.Context, cfg common.OTEL, contextDialer func(ctx c
 	if err != nil {
 		return shutdown, fmt.Errorf("failed to create resource: %w", err)
 	}
+
 	if cfg.TracesEnabled {
-		tp, err := initTracer(ctx, res, conn, cfg)
+		shutdownFunc, err := initTracer(res, cfg)
 		if err != nil {
 			return shutdown, fmt.Errorf("failed to initialize tracer: %w", err)
 		}
 		// Successfully initialized tracer
-		shutdownFuncs = append(shutdownFuncs, tp.Shutdown)
+		shutdownFuncs = append(shutdownFuncs, shutdownFunc)
+		slog.Info("OpenTelemetry tracer initialized")
 	}
 
 	if cfg.MetricsEnabled {
 		// Initialize the meter provider with the same gRPC connection.
 		// This is useful to avoid creating multiple connections to the same endpoint.
-		mp, err := initMeterProvider(ctx, res, conn, cfg)
+		mp, err := initMeterProvider(ctx, res, cfg)
 		if err != nil {
 			return shutdown, fmt.Errorf("failed to initialize meter provider: %w", err)
 		}
@@ -75,49 +73,33 @@ func SetupOTelSDK(ctx context.Context, cfg common.OTEL, contextDialer func(ctx c
 	return shutdown, nil
 }
 
-// Initialize a gRPC connection to be used by both the tracer and meter
-// providers.
-func initConn(endpoint string, contextDialer func(ctx context.Context, addr string) (net.Conn, error)) (*grpc.ClientConn, error) {
-	conn, err := grpc.NewClient(endpoint,
-		grpc.WithContextDialer(contextDialer),
+func initTracer(res *resource.Resource, cfg common.OTEL) (func(context.Context) error, error) {
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracegrpc.NewClient(
+			otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, "")),
+			otlptracegrpc.WithEndpoint(cfg.Endpoint),
+			otlptracegrpc.WithHeaders(cfg.Headers),
+		),
 	)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create gRPC connection to collector: %w", err)
+		log.Fatalf("Failed to create exporter: %v", err)
+		return nil, fmt.Errorf("failed to create exporter: %w", err)
 	}
 
-	return conn, err
-}
-
-// initTracer creates and registers trace provider instance.
-func initTracer(ctx context.Context, res *resource.Resource, conn *grpc.ClientConn, cfg common.OTEL) (*sdktrace.TracerProvider, error) {
-	exp, err := otlptracegrpc.New(ctx,
-		otlptracegrpc.WithGRPCConn(conn),
-		otlptracegrpc.WithEndpoint(cfg.Endpoint),
-		otlptracegrpc.WithHeaders(cfg.Headers),
+	otel.SetTracerProvider(
+		sdktrace.NewTracerProvider(
+			sdktrace.WithSampler(sdktrace.ParentBased(sdktrace.TraceIDRatioBased(cfg.TracesSampleRate))),
+			sdktrace.WithBatcher(exporter),
+			sdktrace.WithResource(res),
+		),
 	)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create trace exporter: %w", err)
-	}
-
-	// Register the trace exporter with a TracerProvider, using a batch
-	// span processor to aggregate spans before export.
-	bsp := sdktrace.NewBatchSpanProcessor(exp)
-	tracerProvider := sdktrace.NewTracerProvider(
-		sdktrace.WithSampler(sdktrace.ParentBased(sdktrace.TraceIDRatioBased(cfg.TracesSampleRate))),
-		sdktrace.WithResource(res),
-		sdktrace.WithSpanProcessor(bsp),
-	)
-	otel.SetTracerProvider(tracerProvider)
-
-	// Set global propagator to tracecontext (the default is no-op).
-	otel.SetTextMapPropagator(propagation.TraceContext{})
-
-	return tracerProvider, nil
+	return exporter.Shutdown, nil
 }
 
 // Initializes an OTLP exporter, and configures the corresponding meter provider.
-func initMeterProvider(ctx context.Context, res *resource.Resource, conn *grpc.ClientConn, cfg common.OTEL) (func(context.Context) error, error) {
-	metricExporter, err := otlpmetricgrpc.New(ctx, otlpmetricgrpc.WithGRPCConn(conn))
+func initMeterProvider(ctx context.Context, res *resource.Resource, cfg common.OTEL) (func(context.Context) error, error) {
+	metricExporter, err := otlpmetricgrpc.New(ctx) //, otlpmetricgrpc.WithGRPCConn(conn))
 	if err != nil {
 		return nil, fmt.Errorf("failed to create metrics exporter: %w", err)
 	}
