fcff01e1b7c4682da3650cff22352235cbc46a25
atavism <paul@getlantern.org>
2025-08-21 04:23:56 -0700

    quick test

--- config/config.go
+++ config/config.go
@@ -9,8 +9,10 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"net/netip"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -23,6 +25,7 @@ import (
 	"github.com/qdm12/reprint"
 	"github.com/sagernet/sing-box/option"
 	singjson "github.com/sagernet/sing/common/json"
+	"github.com/sagernet/sing/common/json/badoption"
 
 	sbx "github.com/getlantern/sing-box-extensions"
 	exO "github.com/getlantern/sing-box-extensions/option"
@@ -291,6 +294,16 @@ func settingWGPrivateKeyInConfig(endpoints []option.Endpoint, privateKey wgtypes
 		switch opts := endpoint.Options.(type) {
 		case *option.WireGuardEndpointOptions:
 			opts.PrivateKey = privateKey.String()
+			if runtime.GOOS == "windows" {
+				if opts.Inet4BindAddress == nil {
+					var addr badoption.Addr
+					nAddr, _ := netip.ParseAddr("0.0.0.0")
+					addr.Build(nAddr)
+					opts.Inet4BindAddress = &addr
+				}
+				opts.Inet6BindAddress = nil
+			}
+
 		case *exO.AmneziaEndpointOptions:
 			opts.PrivateKey = privateKey.String()
 		default:
