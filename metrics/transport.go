0e0855289e57d39816d79f52ccadab461901f8a6
WendelHime <6754291+WendelHime@users.noreply.github.com>
2025-08-27 13:48:16 -0300

    docs: explaining about metris NewRoundTripper

--- metrics/transport.go
+++ metrics/transport.go
@@ -6,6 +6,7 @@ import (
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
+// NewRoundTripper wraps the provided http.RoundTripper with OpenTelemetry instrumentation.
 func NewRoundTripper(original http.RoundTripper) http.RoundTripper {
 	if original == nil {
 		original = http.DefaultTransport
