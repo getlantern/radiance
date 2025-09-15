b3c9ea8e7b86cce0e9bc23a3267f8c8b8e8ed061
atavism <paul@getlantern.org>
2025-09-15 06:31:20 -0700

    update buildOptions

--- vpn/boxoptions.go
+++ vpn/boxoptions.go
@@ -221,6 +221,15 @@ func buildOptions(group, path string) (O.Options, error) {
 	splitTunnelPath := filepath.Join(path, splitTunnelFile)
 	opts.Route.RuleSet[0].LocalOptions.Path = splitTunnelPath
 
+	p := opts.Route.RuleSet[0].LocalOptions.Path
+	if filepath.IsAbs(p) {
+		if rel, err := filepath.Rel(path, p); err == nil {
+			opts.Route.RuleSet[0].LocalOptions.Path = rel
+		} else {
+			opts.Route.RuleSet[0].LocalOptions.Path = splitTunnelFile
+		}
+	}
+
 	slog.Log(nil, internal.LevelTrace, "Updated default options and paths",
 		"cacheFilePath", opts.Experimental.CacheFile.Path,
 		"clashAPIDefaultMode", opts.Experimental.ClashAPI.DefaultMode,
