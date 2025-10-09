991615039ad756af68ed89e6af8a2d025bacb88f
garmr-ulfr <104022054+garmr-ulfr@users.noreply.github.com>
2025-10-09 12:39:21 -0700

    Socket ownership (#165)
    * set socket file ownership to parent directory's uid/gid
    
    * traverse up dirs, returning first non-root uid, ensure close removes socket
    
    * fix typo, use local path value
    
    * add trace logging

--- vpn/ipc/conn_other.go
+++ vpn/ipc/conn_other.go
@@ -5,9 +5,13 @@ package ipc
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"net"
 	"os"
 	"path/filepath"
+	"syscall"
+
+	"github.com/getlantern/radiance/internal"
 )
 
 const (
@@ -15,12 +19,7 @@ const (
 	sockFile = "radiance.sock"
 )
 
-var (
-	sockPath = "radiance.sock" // default to current directory
-
-	uid = os.Getuid()
-	gid = os.Getgid()
-)
+var sockPath = "radiance.sock" // default to current directory
 
 // SetSocketPath sets the path for the Unix domain socket file for client connections.
 func SetSocketPath(path string) {
@@ -34,6 +33,11 @@ func dialContext(_ context.Context, _, _ string) (net.Conn, error) {
 	})
 }
 
+type sockListener struct {
+	net.Listener
+	path string
+}
+
 // listen creates a Unix domain socket listener in the specified directory.
 func listen(path string) (net.Listener, error) {
 	path = filepath.Join(path, sockFile)
@@ -45,9 +49,54 @@ func listen(path string) (net.Listener, error) {
 	if err != nil {
 		return nil, fmt.Errorf("listen %s: %w", path, err)
 	}
+	uid, gid := getNonRootOwner(path)
 	if err := os.Chown(path, uid, gid); err != nil {
 		listener.Close()
-		return nil, fmt.Errorf("chmod %s: %w", path, err)
+		return nil, fmt.Errorf("chown %s: %w", path, err)
 	}
-	return listener, nil
+	return &sockListener{
+		Listener: listener,
+		path:     path,
+	}, nil
+}
+
+func (l *sockListener) Close() error {
+	err := l.Listener.Close()
+	os.Remove(l.path)
+	return err
+}
+
+func getNonRootOwner(path string) (uid, gid int) {
+	uid = os.Getuid()
+	gid = os.Getgid()
+	if uid != 0 {
+		return uid, gid
+	}
+
+	slog.Log(context.Background(), internal.LevelTrace, "searching for non-root owner of", "path", path)
+	for {
+		parentDir := filepath.Dir(path)
+		if parentDir == path || parentDir == "/" {
+			break
+		}
+		path = parentDir
+
+		fInfo, err := os.Stat(path)
+		if err != nil {
+			slog.Log(context.Background(), internal.LevelTrace, "stat error", "path", path, "error", err)
+			continue
+		}
+		stat, ok := fInfo.Sys().(*syscall.Stat_t)
+		if !ok {
+			continue
+		}
+		if int(stat.Uid) != 0 {
+			slog.Log(context.Background(), internal.LevelTrace, "found non-root owner", "path", path, "uid", stat.Uid, "gid", stat.Gid)
+			return int(stat.Uid), int(stat.Gid)
+		}
+	}
+	if slog.Default().Enabled(context.Background(), internal.LevelTrace) {
+		slog.Warn("falling back to root owner for", "path", path)
+	}
+	return uid, gid
 }
