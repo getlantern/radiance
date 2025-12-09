e3b6b4c3bf85af7fce96430bbb2ff19a81f2478d
garmr <pdixon117@gmail.com>
2025-12-09 13:24:14 -0800

    set libbox working path on non-windows platforms

--- vpn/tunnel.go
+++ vpn/tunnel.go
@@ -133,15 +133,17 @@ func (t *tunnel) init(opts O.Options, dataPath string, platIfce libbox.PlatformI
 
 	// setup libbox service
 	setupOpts := &libbox.SetupOptions{
-		BasePath:    dataPath,
-		WorkingPath: dataPath,
-		TempPath:    filepath.Join(dataPath, "temp"),
+		BasePath: dataPath,
+		TempPath: filepath.Join(dataPath, "temp"),
 	}
-	if common.Platform == "android" {
+	switch common.Platform {
+	case "windows":
+		setupOpts.WorkingPath = dataPath
+	case "android":
 		setupOpts.FixAndroidStack = true
 	}
-	slog.Log(nil, internal.LevelTrace, "Setting up libbox", "setup_options", setupOpts)
 
+	slog.Log(nil, internal.LevelTrace, "Setting up libbox", "setup_options", setupOpts)
 	if err := libbox.Setup(setupOpts); err != nil {
 		return fmt.Errorf("setup libbox: %w", err)
 	}
