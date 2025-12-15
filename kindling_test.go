a717c5678e7a0ac1fdece55c8220fc9a8afaa81a
WendelHime <6754291+WendelHime@users.noreply.github.com>
2025-12-15 16:44:35 -0300

    feat: updating testing report integration test

--- kindling_test.go
+++ kindling_test.go
@@ -7,23 +7,17 @@ import (
 	"context"
 	"crypto/rand"
 	"encoding/base64"
-	"io"
 	"log/slog"
 	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
-	"time"
 
-	"github.com/getlantern/dnstt"
 	"github.com/getlantern/kindling"
 	"github.com/getlantern/radiance/common"
 	"github.com/getlantern/radiance/common/reporting"
-	"github.com/getlantern/radiance/config"
-	"github.com/getlantern/radiance/events"
 	"github.com/getlantern/radiance/fronted"
 	"github.com/getlantern/radiance/issue"
-	"github.com/getlantern/radiance/traces"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -56,99 +50,6 @@ func TestKindlingIntegrations(t *testing.T) {
 	userID := ""
 
 	tests := []testCase{
-		{
-			name:        "DNSTT Kindling",
-			description: "dnstt testing",
-			userID:      userID,
-			locale:      "en-US",
-			email:       email,
-			country:     "US",
-			setup: func(
-				ctx context.Context,
-				t *testing.T,
-				dataDir string,
-				logger *slogWriter,
-			) (kindling.Kindling, func(k kindling.Kindling, client *http.Client)) {
-				dnsTunnel, err := dnstt.NewDNSTT(
-					dnstt.WithTunnelDomain("t.iantem.io"),
-					dnstt.WithDoH("https://cloudflare-dns.com/dns-query"),
-					dnstt.WithPublicKey("405eb9e22d806e3a0a8e667c6665a321c8a6a35fa680ed814716a66d7ad84977"),
-				)
-				require.NoError(t, err)
-
-				dnsTunnel2, err := dnstt.NewDNSTT(
-					dnstt.WithTunnelDomain("t.iantem.io"),
-					dnstt.WithDoH("https://dns.adguard-dns.com/dns-query"),
-					dnstt.WithPublicKey("405eb9e22d806e3a0a8e667c6665a321c8a6a35fa680ed814716a66d7ad84977"),
-				)
-				require.NoError(t, err)
-
-				dnsTunnel3, err := dnstt.NewDNSTT(
-					dnstt.WithTunnelDomain("t.iantem.io"),
-					dnstt.WithDoH("https://dns.google/dns-query"),
-					dnstt.WithPublicKey("405eb9e22d806e3a0a8e667c6665a321c8a6a35fa680ed814716a66d7ad84977"),
-				)
-				require.NoError(t, err)
-
-				k := kindling.NewKindling(
-					"radiance",
-					kindling.WithPanicListener(reporting.PanicListener),
-					kindling.WithLogWriter(logger),
-					kindling.WithDNSTunnel(dnsTunnel),
-					kindling.WithDNSTunnel(dnsTunnel2),
-					kindling.WithDNSTunnel(dnsTunnel3),
-				)
-
-				after := func(k kindling.Kindling, client *http.Client) {
-					events.Subscribe(func(e config.NewDNSTTConfigEvent) {
-						slog.Info("updating kindling with latest dnstt config")
-						// replacing dnstt roundtripper and making http client replace transports
-						k.ReplaceRoundTripGenerator("dnstt", e.New.NewRoundTripper)
-						// re-create race transport
-						k.NewHTTPClient()
-						// add trace roundtripper again
-						client.Transport = traces.NewRoundTripper(
-							traces.NewHeaderAnnotatingRoundTripper(client.Transport),
-						)
-					})
-					config.DNSTTConfigUpdate(
-						ctx,
-						"https://raw.githubusercontent.com/getlantern/radiance/main/config/dnstt.yml.gz",
-						client,
-						12*time.Hour,
-					)
-					time.Sleep(5 * time.Second)
-				}
-
-				return k, after
-			},
-		},
-		{
-			name:        "AMP Kindling",
-			description: "amp testing",
-			userID:      userID,
-			locale:      "en-US",
-			email:       email,
-			country:     "US",
-			setup: func(
-				ctx context.Context,
-				t *testing.T,
-				dataDir string,
-				logger *slogWriter,
-			) (kindling.Kindling, func(k kindling.Kindling, client *http.Client)) {
-				ampClient, err := fronted.NewAMPClient(ctx, logger, ampPublicKey)
-				require.NoError(t, err)
-
-				k := kindling.NewKindling(
-					"radiance",
-					kindling.WithPanicListener(reporting.PanicListener),
-					kindling.WithLogWriter(logger),
-					kindling.WithAMPCache(ampClient),
-				)
-
-				return k, nil
-			},
-		},
 		{
 			name:        "Fronted Kindling",
 			description: "fronted testing",
@@ -183,30 +84,28 @@ func TestKindlingIntegrations(t *testing.T) {
 			name:        "Proxyless Kindling",
 			description: "proxyless testing",
 			userID:      userID,
-			locale:      "en-US",
+			locale:      "pt-BR",
 			email:       email,
-			country:     "US",
+			country:     "BR",
 			setup: func(
 				_ context.Context,
-				_ *testing.T,
-				_ string,
+				t *testing.T,
+				dataDir string,
 				logger *slogWriter,
 			) (kindling.Kindling, func(k kindling.Kindling, client *http.Client)) {
 				k := kindling.NewKindling(
 					"radiance",
 					kindling.WithPanicListener(reporting.PanicListener),
 					kindling.WithLogWriter(logger),
-					kindling.WithProxyless("df.iantem.io", "detectportal.firefox.com"),
+					kindling.WithProxyless("df.iantem.io", "api.getiantem.org"),
 				)
 
 				return k, nil
 			},
 		},
 	}
-
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
 			dataDir := t.TempDir()
 			kindlingLogger := &slogWriter{Logger: slog.Default()}
 
@@ -216,7 +115,7 @@ func TestKindlingIntegrations(t *testing.T) {
 			k, after := tc.setup(ctx, t, dataDir, kindlingLogger)
 
 			httpClientWithTimeout := k.NewHTTPClient()
-			httpClientWithTimeout.Timeout = 1 * time.Minute
+			httpClientWithTimeout.Timeout = common.DefaultHTTPTimeout
 
 			if after != nil {
 				after(k, httpClientWithTimeout)
@@ -230,7 +129,7 @@ func TestKindlingIntegrations(t *testing.T) {
 
 			t.Run("reporting issue should work", func(t *testing.T) {
 				// ~15MB payload
-				const size = 15 * 1000000
+				const size = 10 * 1000000
 				// Base64 inflates: 3 bytes → 4 chars
 				raw := make([]byte, size*3/4+3) // +3 to avoid truncation issues
 
@@ -256,27 +155,6 @@ func TestKindlingIntegrations(t *testing.T) {
 					tc.email,
 					tc.country,
 				))
-			})
-
-			t.Run("sending request to detectportal should succeed", func(t *testing.T) {
-				req, err := http.NewRequest(
-					http.MethodPost,
-					"https://detectportal.firefox.com/success.txt",
-					http.NoBody,
-				)
-				require.NoError(t, err)
-
-				response, err := httpClientWithTimeout.Do(req)
-				require.NoError(t, err)
-				defer response.Body.Close()
-
-				assert.Equal(t, http.StatusOK, response.StatusCode)
-
-				content, err := io.ReadAll(response.Body)
-				require.NoError(t, err)
-
-				t.Log(response.StatusCode)
-				t.Log(string(content))
 			})
 		})
 	}
