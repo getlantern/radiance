c4fed7d4404ed7163fea0d8b96d3a1fd250d6e02
WendelHime <6754291+WendelHime@users.noreply.github.com>
2026-01-14 15:55:29 -0300

    fix: removing defaultOptions since they're not useful anymore and simplify a lot the client

--- kindling/client.go
+++ kindling/client.go
@@ -2,8 +2,6 @@ package kindling
 
 import (
 	"context"
-	"fmt"
-	"io"
 	"log/slog"
 	"net/http"
 	"path/filepath"
@@ -18,74 +16,66 @@ import (
 )
 
 var (
-	k kindling.Kindling
-	// defaultOptions generally does not change after the first time
-	// or if they change, it's handled internally
-	defaultOptions = make([]kindling.Option, 0)
-	mutexOptions   sync.Mutex
+	k             kindling.Kindling = newKindling()
+	kindlingMutex sync.Mutex
+	stopUpdater   func()
 )
 
 // HTTPClient returns a http client with kindling transport
 func HTTPClient() *http.Client {
-	mutexOptions.Lock()
-	defer mutexOptions.Unlock()
-
-	if k == nil {
-		mutexOptions.Unlock()
-		err := NewKindling(context.Background(), settings.GetString(settings.DataPathKey), &slogWriter{Logger: slog.Default()})
-		if err != nil {
-			slog.Error("failed to build kindling", slog.Any("error", err))
-			return &http.Client{}
-		}
-		mutexOptions.Lock()
-	}
-
 	httpClient := k.NewHTTPClient()
 	httpClient.Timeout = common.DefaultHTTPTimeout
 	httpClient.Transport = traces.NewRoundTripper(traces.NewHeaderAnnotatingRoundTripper(httpClient.Transport))
 	return httpClient
 }
 
+func Close() {
+	if stopUpdater != nil {
+		stopUpdater()
+	}
+}
+
 // SetKindling sets the kindling method used for building the HTTP client
 // This function is useful for testing purposes.
 func SetKindling(a kindling.Kindling) {
-	mutexOptions.Lock()
-	defer mutexOptions.Unlock()
+	kindlingMutex.Lock()
+	defer kindlingMutex.Unlock()
 	k = a
 }
 
-// NewKindling build a kindling client and bootstrap this package
-func NewKindling(ctx context.Context, dataDir string, logger io.Writer) error {
-	mutexOptions.Lock()
-	defer mutexOptions.Unlock()
+// newKindling build a kindling client and bootstrap this package
+func newKindling() kindling.Kindling {
+	dataDir := settings.GetString(settings.DataPathKey)
+	logger := &slogWriter{Logger: slog.Default()}
+	updaterCtx, cancel := context.WithCancel(context.Background())
 
-	if len(defaultOptions) == 0 {
-		defaultOptions = append(defaultOptions,
-			kindling.WithPanicListener(reporting.PanicListener),
-			kindling.WithLogWriter(logger),
-			// Most endpoints use df.iantem.io, but for some historical reasons
-			// "pro-server" calls still go to api.getiantem.org.
-			kindling.WithProxyless("df.iantem.io", "api.getiantem.org"),
-		)
-
-		f, err := fronted.NewFronted(reporting.PanicListener, filepath.Join(dataDir, "fronted_cache.json"), logger)
-		if err != nil {
-			return fmt.Errorf("failed to create fronted: %w", err)
-		} else {
-			defaultOptions = append(defaultOptions, kindling.WithDomainFronting(f))
-		}
-
-		ampClient, err := fronted.NewAMPClient(ctx, dataDir, logger)
-		if err != nil {
-			return fmt.Errorf("failed to create amp client: %w", err)
-		} else {
-			// Kindling will skip amp transports if the request has a payload larger than 6kb
-			defaultOptions = append(defaultOptions, kindling.WithAMPCache(ampClient))
-		}
+	kindlingOptions := []kindling.Option{
+		kindling.WithPanicListener(reporting.PanicListener),
+		kindling.WithLogWriter(logger),
+		// Most endpoints use df.iantem.io, but for some historical reasons
+		// "pro-server" calls still go to api.getiantem.org.
+		kindling.WithProxyless("df.iantem.io", "api.getiantem.org"),
 	}
 
-	k = kindling.NewKindling("radiance", defaultOptions...)
-	return nil
+	f, err := fronted.NewFronted(reporting.PanicListener, filepath.Join(dataDir, "fronted_cache.json"), logger)
+	if err != nil {
+		slog.Error("failed to create fronted client", slog.Any("error", err))
+	} else {
+		kindlingOptions = append(kindlingOptions, kindling.WithDomainFronting(f))
+	}
+
+	ampClient, err := fronted.NewAMPClient(updaterCtx, dataDir, logger)
+	if err != nil {
+		slog.Error("failed to create amp client", slog.Any("error", err))
+	} else {
+		// Kindling will skip amp transports if the request has a payload larger than 6kb
+		kindlingOptions = append(kindlingOptions, kindling.WithAMPCache(ampClient))
+	}
+
+	kindlingMutex.Lock()
+	defer kindlingMutex.Unlock()
+	stopUpdater = cancel
+	return kindling.NewKindling("radiance", kindlingOptions...)
 }
 
 type slogWriter struct {
