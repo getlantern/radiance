c77ea6190edf89bf654aea438da7d9cb9027a304
WendelHime <6754291+WendelHime@users.noreply.github.com>
2026-01-19 17:46:38 -0300

    fix: removing test closing chan

--- kindling/dnstt/parser_test.go
+++ kindling/dnstt/parser_test.go
@@ -74,7 +74,6 @@ dnsttConfigs:
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			updated := make(chan struct{})
-			defer close(updated)
 			if tt.expectUpdate {
 				events.Subscribe(func(e DNSTTUpdateEvent) {
 					assert.NotEmpty(t, e.YML)
