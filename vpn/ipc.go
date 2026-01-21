dd9ece51ab7ee4091cd5cf86b050d0bf09803187
garmr <pdixon117@gmail.com>
2026-01-21 15:27:26 -0800

    logging, only set ipcserver on start nil error

--- vpn/ipc.go
+++ vpn/ipc.go
@@ -1,10 +1,15 @@
 package vpn
 
 import (
+	"fmt"
+	"log/slog"
 	"sync"
 
 	"github.com/sagernet/sing-box/experimental/libbox"
 
+	"github.com/getlantern/radiance/common"
+	"github.com/getlantern/radiance/common/settings"
+	"github.com/getlantern/radiance/internal"
 	"github.com/getlantern/radiance/vpn/ipc"
 )
 
@@ -15,18 +20,28 @@ var (
 
 // InitIPC initializes and starts the IPC server. If the server is already running, it returns the
 // existing instance.
-func InitIPC(dataPath, logPath, logLevel string, provider func() libbox.PlatformInterface) (*ipc.Server, error) {
+func InitIPC(dataPath, logPath, logLevel string, platformIfce libbox.PlatformInterface) (*ipc.Server, error) {
 	ipcMu.Lock()
 	defer ipcMu.Unlock()
 	if ipcServer != nil {
 		// already started
+		slog.Log(nil, internal.LevelTrace, "IPC server already started")
 		return ipcServer, nil
 	}
 
-	var platformIfce libbox.PlatformInterface
-	if provider != nil {
-		platformIfce = provider()
+	if err := common.InitReadOnly(dataPath, logPath, logLevel); err != nil {
+		return nil, fmt.Errorf("initialize common package: %w", err)
 	}
-	ipcServer = ipc.NewServer(NewTunnelService(dataPath, logPath, logLevel, platformIfce))
-	return ipcServer, ipcServer.Start(dataPath)
+	if path := settings.GetString(settings.DataPathKey); path != "" && path != dataPath {
+		dataPath = path
+	}
+
+	server := ipc.NewServer(NewTunnelService(dataPath, slog.Default().With("service", "ipc"), platformIfce))
+	slog.Debug("starting IPC server")
+	if err := server.Start(dataPath); err != nil {
+		slog.Error("failed to start IPC server", "error", err)
+		return nil, err
+	}
+	ipcServer = server
+	return server, nil
 }
