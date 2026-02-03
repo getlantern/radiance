bc3d7358f6e7a3836d0b15cb90ecb75dec9a7396
garmr <pdixon117@gmail.com>
2026-02-02 16:45:37 -0800

    remove testing/debug logs

--- vpn/ipc/usr.go
+++ vpn/ipc/usr.go
@@ -2,12 +2,9 @@ package ipc
 
 import (
 	"context"
-	"log/slog"
 	"os/exec"
 	"runtime"
 	"time"
-
-	"github.com/getlantern/radiance/internal"
 )
 
 type usrKey struct{}
@@ -36,19 +33,8 @@ func canSudo(usr string) bool {
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
-	slog.Log(nil, internal.LevelTrace, "Checking sudo access for user", "user", usr)
 	if err := exec.CommandContext(ctx, "sudo", "-n", "--other-user="+usr, "--list").Run(); err != nil {
 		return false
 	}
 	return true
 }
-
-// func ContextWithID(ctx context.Context, id ID) context.Context {
-// 	return context.WithValue(ctx, (*idKey)(nil), id)
-// }
-//
-// func IDFromContext(ctx context.Context) (ID, bool) {
-// 	id, loaded := ctx.Value((*idKey)(nil)).(ID)
-// 	return id, loaded
-// }
-//
