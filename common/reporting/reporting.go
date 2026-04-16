748634a53728ea0bef753c2c51d9cdc2201bba09
Jay <110402935+jay-418@users.noreply.github.com>
2026-04-15 20:13:04 -0600

    implement pr fixes

--- common/reporting/reporting.go
+++ common/reporting/reporting.go
@@ -3,12 +3,15 @@ package reporting
 import (
 	"context"
 	"log/slog"
-)
+	"runtime/debug"
 
-const levelPanic = slog.LevelError + 8
+	"github.com/getlantern/radiance/internal"
+)
 
 // PanicListener logs a panic message. It is passed as a callback to
 // libraries that need a panic notification hook.
 func PanicListener(msg string) {
-	slog.Log(context.Background(), levelPanic, msg)
+	slog.Log(context.Background(), internal.LevelPanic, msg,
+		"stack", string(debug.Stack()),
+	)
 }
